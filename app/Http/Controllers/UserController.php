0d175e471f932a0fcf0136396acb412a9f520a7b
mouad002 <elcosto02@gmail.com>
2023-08-24 13:50:19 +0100

    maintenance of get studentds API and block the mail part in add user API

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -69,25 +69,37 @@ class UserController extends Controller
 
     public function getStudents()
     {
-        $student_courses = DB::select('select u.id as user_id, u.full_name as user_full_name, c.id as course_id, c.name as course_name, g.id as group_id from users u join students_groups sg on u.id = sg.student join `groups` g on sg.group = g.id join courses c on c.id = g.course;');
-        $result = [];
-        foreach($student_courses as $item)
+        $students = User::where('role',3)->get(['id','full_name']);
+        foreach($students as $student)
         {
-            $sessions = DB::select('select s.id , s.date, s.time from sessions s join presences p on s.id = p.`session` where p.student = ? and s.`group` = ?;',[$item->user_id,$item->group_id]);
-            $pre_score = DB::select('select score from groups_projects gp join students_progresses sp on gp.id = sp.group_project where gp.`group` = ? and sp.student = ?;',[$item->group_id,$item->user_id]);
-            $score = 0;
-            if($pre_score!=null)
+            $student_courses = DB::select('select  c.id as course_id, c.name as course_name, g.id as group_id from users u join students_groups sg on u.id = sg.student join `groups` g on sg.group = g.id join courses c on c.id = g.course where u.id = ?;',[$student->id]);
+            $courses = [];
+            foreach($student_courses as $item)
             {
-                $score = $pre_score[0]->score;
+                $sessions = DB::select('select s.id , s.date, s.time from sessions s join presences p on s.id = p.`session` where p.student = ? and s.`group` = ?;',[$student->id,$item->group_id]);
+                $pre_score = DB::select('select score from groups_projects gp join students_progresses sp on gp.id = sp.group_project where gp.`group` = ? and sp.student = ?;',[$item->group_id,$student->id]);
+                $score = 0;
+                if($pre_score!=null)
+                {
+                    $score = $pre_score[0]->score;
+                }
+                array_push($courses,[
+                    'id'=>$item->course_id,
+                    'name'=>$item->course_name,
+                    'presence'=> $sessions,
+                    'score'=> $score
+                ]);                
+                // array_push($result,[
+                //     'full_name' => $item->user_full_name,
+                //     'course' => $item->course_name,
+                //     'presence' => $sessions,
+                //     'score' => $score 
+                // ]);
             }
-            array_push($result,[
-                'full_name' => $item->user_full_name,
-                'course' => $item->course_name,
-                'presence' => $sessions,
-                'score' => $score 
-            ]);
+            $student->courses = $courses;
         }
-        return response($result);
+        
+        return response($students);
     }
 
     public function getTeachers()
